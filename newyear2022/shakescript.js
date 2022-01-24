1af2aa017ba267461bab5382fb8ff5cad36cab55
Franz-Kim <71546458+Franz-Kim@users.noreply.github.com>
2022-01-24 16:11:10 +0900

    newyear

--- newyear2022/shakescript.js
+++ newyear2022/shakescript.js
@@ -1,5 +1,14 @@
+var myShakeEvent = new Shake({
+    threshold: 15, // optional shake strength threshold
+    timeout: 1000 // optional, determines the frequency of event generation
+});
+myShakeEvent.start();
+
 window.addEventListener('shake', shakeEventDidOccur, false);
 
+//function to call when shake occurs
 function shakeEventDidOccur () {
- alert("흔들??")  
+
+    //put your own code here etc.
+    alert('shake!');
 }
